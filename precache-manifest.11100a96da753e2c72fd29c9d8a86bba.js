2ae115b8a0e62347b793391a7ff45852e0128cd3
vincephung <vincentphung43@yahoo.com>
2020-09-22 21:02:29 -0400

    Updates

---
+++ precache-manifest.11100a96da753e2c72fd29c9d8a86bba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d15c229e53633ec20fb3764d52b20d",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "671e235a216a0d130dc7",
+    "url": "/battleship/static/css/main.747b5e12.chunk.css"
+  },
+  {
+    "revision": "dc88c0911d37bf1b1d4e",
+    "url": "/battleship/static/js/2.cff93398.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.cff93398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "671e235a216a0d130dc7",
+    "url": "/battleship/static/js/main.6cedaaf1.chunk.js"
+  },
+  {
+    "revision": "1576b120f8ea920fbd40",
+    "url": "/battleship/static/js/runtime-main.9e463cc9.js"
+  }
+]);
